5be7560a90d2135ca4b272896e2c37ca10389668
ZackyXOXO <seven.sixerz@gmail.com>
2023-03-24 07:34:04 +0700

    lang

--- js/box.js
+++ js/box.js
@@ -2,7 +2,7 @@ let text;
 let person = prompt("Masukkan Nama Anda:", "");
 
 if (person == null || person == ""){
-    window.location.href= "pages/404.html";
+    window.location.href= "zackysaktiawan.github.io/pages/404.html";
 } else {
     text = "Aloowww " + person + " Selamat Datang ";
 }
